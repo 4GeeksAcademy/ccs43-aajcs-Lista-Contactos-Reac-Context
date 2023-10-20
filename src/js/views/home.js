338ef181c9733c906dda0bbfd10712a4a4b6bb41
Alfredo Castillo <98472648+aajcs@users.noreply.github.com>
2023-10-20 04:25:22 +0000

    subiendo para entrega

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,81 @@
-import React from "react";
+import React, { useContext } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import { Link } from "react-router-dom";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+  const { store, actions } = useContext(Context);
+  console.log(store.contactList);
+  return (
+    <>
+      <div className="container-fluid">
+        <h1 className="text-center">Tarjeta de contactos</h1>
+        {store.contactList.map((contact) => {
+          return (
+            <div key={contact.id} className=" mt-5">
+              <ul className="list-group container">
+                <li
+                  className="list-group-item "
+                  style={{ background: "white" }}
+                >
+                  <div className="d-flex justify-content-between">
+                    <div className="d-flex">
+                      <div>
+                        <img
+                          src="https://picsum.photos/200/300"
+                          alt="..."
+                          className="rounded-circle contact-image"
+                          height="100"
+                          width="100"
+                        />
+                      </div>
+                      <div className="mx-5">
+                        <h6>{contact.full_name}</h6>
+                        <span>
+                          <p>
+                            {" "}
+                            <i className="me-2 fa-solid fa-location-dot"></i>
+                            {contact.address}
+                          </p>
+                        </span>
+                        <span>
+                          <p>
+                            <i className="me-2 fa-solid fa-phone-flip"></i>
+                            {contact.phone}
+                          </p>
+                        </span>
+                        <span>
+                          <p>
+                            <i className="me-2 fa-solid fa-envelope"></i>
+                            {contact.email}
+                          </p>
+                        </span>
+                      </div>
+                    </div>
+                    <div>
+                      <Link to={`/contact/${contact.id}`}>
+                        <button className="btn btn-primary mx-3 ">
+                          <i className="fa-solid fa-pen-to-square"></i>
+                        </button>
+                      </Link>
+
+                      <button
+                        className="btn btn-danger "
+                        onClick={() => {
+                          actions.removeContacts(contact.id);
+                        }}
+                      >
+                        <i className="fa-solid fa-trash-can"></i>
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          );
+        })}{" "}
+      </div>
+    </>
+  );
+};
